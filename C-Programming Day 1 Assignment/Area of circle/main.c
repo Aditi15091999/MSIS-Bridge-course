c2a859cc014877f294d2a693d745c86f54805584
Aditi <65910955+Aditi15091999@users.noreply.github.com>
2021-10-08 20:00:30 +0530

    Add files via upload

---
+++ C-Programming Day 1 Assignment/Area of circle/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//# define pi 3.14;
+int main(){
+    float r,area;
+    printf("Enter the radius of the circle:");
+    scanf("%f",&r);
+    area=3.14*r*r;
+    printf("The area of the circle is %.2f\n",area);
+    return 0;
+
+}
